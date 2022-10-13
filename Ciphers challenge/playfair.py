3a75b3e4f192bf72c1dca8837cbe1bc263bdf392
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2022-10-13 10:17:52 +0100

    Update playfair.py

--- Ciphers challenge/playfair.py
+++ Ciphers challenge/playfair.py
@@ -1,12 +1,14 @@
 print("hello world")
 def menu():
+    grids = []
     print("""
     ---------------WELCOME-----------------------
     Please choose an option:
-    1. Create grids
+    1. Create grids (Type C)
     2. Encrypt (Type E)
     3. Decrypt (Type D)
     4. Quit (Type Q)""")
+    
 
 
 def secret1(word):
@@ -33,11 +35,16 @@ def secret2(word):
 def prin_grid(grid):
     for x in range(0,(len(grid)//5)):
         print(grid[5*x:5*(x+1)])
+
+
+def create_grid():
+    grid1st = secret1(input("Please enter the first secret word:"))
+    grid2nd = secret2(input("Please enter the 2nd word"))
 # Main code
 
 
 grid1st = secret1(input("Please enter the first secret word:"))
-grid2nd = secret2(input("Please enter the 2nd word"))
+
 print("")
 prin_grid(grid1st)
 print("")
