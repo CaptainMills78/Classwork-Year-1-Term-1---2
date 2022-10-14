dfa4ef660abf3ac3ea2b44bd54ecd8fbe5c350ce
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2022-10-14 13:46:56 +0100

    Update playfair.py

--- Ciphers challenge/playfair.py
+++ Ciphers challenge/playfair.py
@@ -1,13 +1,20 @@
-print("hello world")
 def menu():
-    grids = []
-    print("""
+    grids = ""
+    mode = input("""
     ---------------WELCOME-----------------------
     Please choose an option:
     1. Create grids (Type C)
     2. Encrypt (Type E)
     3. Decrypt (Type D)
-    4. Quit (Type Q)""")
+    4. Quit (Type Q)""").upper()
+    if mode == "C" or mode == 1:
+        grids = create_grid()
+    elif mode == "E" or mode == 2:
+        if grids == "":
+            print("You have not set up any grids, we will do that now...")
+            grids = create_grid()
+        else:
+            
 
 
 
@@ -27,25 +34,28 @@ def secret2(word):
     for x in range(len(word)):
         if word[x] not in reverse:
             reverse = reverse + word[x]
-    for x in range(1, len(reverse)+1):
+    for x in range(1, len(reverse) + 1):
         grid2 = grid2 + reverse[-1 * x]
     return grid2
 
 
 def prin_grid(grid):
-    for x in range(0,(len(grid)//5)):
-        print(grid[5*x:5*(x+1)])
+    for x in range(0, (len(grid) // 5)):
+        print(grid[5 * x:5 * (x + 1)])
+    return
 
 
 def create_grid():
     grid1st = secret1(input("Please enter the first secret word:"))
     grid2nd = secret2(input("Please enter the 2nd word"))
+    print("")
+    prin_grid(grid1st)
+    print("")
+    prin_grid(grid2nd)
+    grids = [grid1st, grid2nd]
+    return grids
+
+
 # Main code
 
-
-grid1st = secret1(input("Please enter the first secret word:"))
-
-print("")
-prin_grid(grid1st)
-print("")
-prin_grid(grid2nd)
+print("hello world")
