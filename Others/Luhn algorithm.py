10a848a313105f587c6541f60b75b16d51d17549
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2022-11-17 09:50:29 +0000

    Added Luhn Algorithm

--- Others/Luhn algorithm.py
+++ Others/Luhn algorithm.py
@@ -1,5 +1,21 @@
 def getTotal(card_no):
-    total = 70  # Test algorithm
+    total = 0
+    for x in range(len(card_no)):
+        if card_no[x].isdigit():
+            if (x+1) % 2 != 0:
+                total += int(card_no[x])
+            else:
+                temp = int(card_no[x]) * 2
+                if temp > 9:
+                    temp2 = 0
+                    for y in str(temp):
+                        temp2 += int(y)
+                    temp = temp2
+                total += temp
+        else:
+            total = 1
+            print("Card number is not a number...")
+            return total
     return total
 
 
@@ -15,7 +31,7 @@ def main():
     count = 0
     valid = False
     while count < 3:
-        number = str("Please enter your card number:")
+        number = str(input("Please enter your card number:"))
         tot = getTotal(number)
         valid = isValid(tot)
         if not valid:
