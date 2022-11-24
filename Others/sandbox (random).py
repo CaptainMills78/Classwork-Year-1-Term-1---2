d3fae309be1b0cd528ccd7cdd2bae88ad6e1679d
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2022-11-24 19:56:16 +0000

    sandbox

---
+++ Others/sandbox (random).py
@@ -0,0 +1,15 @@
+import random
+import string
+lowercase = string.ascii_lowercase
+randomLetters = ['q', 's', 'g', 'b', 'j', 'i', 'l', 'b', 'k', 'q']
+#for x in range(10):
+#    randomLetters.append(lowercase[random.randint(0, len(lowercase)-1)])
+print(randomLetters)
+randomLetters.remove("q")
+print(randomLetters)
+randomLetters.remove("q")
+print(randomLetters)
+
+
+
+
