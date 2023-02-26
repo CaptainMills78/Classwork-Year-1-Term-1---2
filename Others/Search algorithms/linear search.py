69c3da2f8246bb3b54522bdb3c156b44cbf47452
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2023-02-26 13:47:41 +0000

    sandbox

--- Others/Search algorithms/linear search.py
+++ Others/Search algorithms/linear search.py
@@ -1,16 +1,17 @@
 def linear(values, target):
     x = 0
     found = False
+    position = -1
     while x < len(values) and not found:  # Need condition to finish once length has been reached to avoid
         # index-out-of-range error
         # Need not found condition unless duplication is possible - this will currently stop at finding one instance
-        if values[x] == target:
+        if values[x] == target: # If item found
             found = True
-        else:
+        else: # Increment
             x += 1
-    print("In List? - " + str(found))
-    if found:
-        print("Target at position: " + str(x))
+    if found:     # If item exists
+        position = x     # Set position
+    return found, position     # Return whether it has been found and the position as an optional result
 
 
 if __name__ == "__main__":
