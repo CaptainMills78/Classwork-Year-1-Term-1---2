cb7e0cffea91407fb85d0c42deb738b9939c3369
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2023-03-31 14:15:20 +0100

    Updated Classwork

--- Others/Encryption/caesar cipher/caesar.py
+++ Others/Encryption/caesar cipher/caesar.py
@@ -1,5 +1,7 @@
 def encrypt(plainText, placeShift):
-    initial = {"A":0, "B":1, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9, "J":10, "K":11, "L":12, "M":13, "N":14, "O":15, "P":16, "Q":17, "R":18, "S":19, "T":20, "U":21, "V":22, "W":23, "X":24, "Y":25, "Z":26}
+    initial = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10, "L": 11,
+               "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17, "S": 18, "T": 19, "U": 20, "V": 21, "W": 22,
+               "X": 23, "Y": 24, "Z": 25}
     if placeShift < 0:
         forwards = False
         placeShift = abs(placeShift)
@@ -23,9 +25,9 @@ def encrypt(plainText, placeShift):
 
 
 def decrypt(cipherText, placeShift):
-    initial = {"A": 0, "B": 1, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
-               "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23,
-               "X": 24, "Y": 25, "Z": 26}
+    initial = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10, "L": 11,
+               "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17, "S": 18, "T": 19, "U": 20, "V": 21, "W": 22,
+               "X": 23, "Y": 24, "Z": 25}
     if placeShift < 0:
         forwards = True
         placeShift = abs(placeShift)
