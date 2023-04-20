118aa212d1bdeb4a4462aa66aa5ff79295270e43
CapMills78 <113183884+CaptainMills78@users.noreply.github.com>
2023-04-20 10:15:13 +0100

    Updated Classwork

--- Others/Object Oriented Programming/OOP Demo MyPets.py
+++ Others/Object Oriented Programming/OOP Demo MyPets.py
@@ -1,79 +1,84 @@
 # Demo on OOP
-class Pets():
-          def __init__(self , givenName, givenAge):
-                    self.name = givenName
-                    self.__age = givenAge  # age is private
+class Pets():  # Write the master class then test before continuing - if this doesn't work, the subclasses won't work
+    def __init__(self, givenName, givenAge):
+        self.name = givenName  # name is public
+        self.__age = givenAge  # age is private  -  can only be accessed within the class
+        # If accessed outside of the class, it will be seen as undefined - print(myPet.age) --> error
 
-          def describe(self):
-                    return  self.name + " I am " + str(self.__age) + " years old"
+    def describe(self):
+        return self.name + " I am " + str(self.__age) + " years old"
 
-                    
-          def speak(self, sound):
-                    print( self.name + " says " + sound)
+    def speak(self, sound):  # Sound can be text
+        print(self.name + " says " + sound)  # Sound is not an attribute, so does not need self.____
 
-                    
-          def sleep(self): # all Pets sleep silently
-                    print("Sleeping Silently")
+    def sleep(self):  # all Pets sleep silently by default
+        print("Sleeping Silently")
 
-          def ask(self, question):
-                    return(question)
+    def ask(self, question):  # question is not an attribute
+        return (question)  # Nothing will happen - nothing is done with the question
 
-          def set_age(self, newAge):
-              self.__age = newAge
+    # Encapsulation functions:
 
-          def get_age(self):
-              return self.__age
+    def set_age(self, newAge):  # Allows the changing of the age attribute - Setter
+        self.__age = newAge  # Have to pass the new value to change to - attribute set to the new value
+        # Conditions for values put here (e.g. if newAge > 20:)
+
+    def get_age(self):  # Allows reading of the age attribute - Getter
+        return self.__age  # Returns the value of the attribute - must be caught by a variable / other function to be
+        # used
 
 
-class Dog(Pets):# dog inherits  all attributes and methods from Pets
+class Dog(Pets):  # dog inherits  all attributes and methods from Pets
 
-          def __init__(self, givenspecies, givenname, givenage):
-                    self.species = givenspecies
-                    number_of_legs = 4  # all dogs have 4 legs
-                    super().__init__(givenname, givenage)
+    def __init__(self, givenspecies, givenname,
+                 givenage):  # givenname / givenage not spelled same as super class arguments
+        self.species = givenspecies
+        number_of_legs = 4  # all dogs have 4 legs, so set as a constant
+        super().__init__(givenname, givenage)  # Create a Pet, then make it a Dog
 
-          def bark(self):
-                    print(" Woof .... Woof...Woof")
-         
-          
-class Cat(Pets): # Cat also inherits  attributes and methods from pets
+    def bark(self):
+        print(" Woof .... Woof...Woof")
 
-          def __init__(self, givenbread,givenname, givenage):
-                    self.bread = givenbread
-                    number_of_legs = 4  # all cats have 4 legs
-                    super().__init__(givenname, givenage)
 
-          def meow(self):
-                    print(" Meow .... meow..... Meow")
-                    
-          def sleep(self):
-                    print("my name is  " + self.name +
-                          " I am closing my eyes slowly and ... Snore....Snore....Snore")
+class Cat(Pets):  # Cat also inherits  attributes and methods from pets
 
-if __name__ == '__main__':                    
-        myDog = Dog("Bull dog", "Max", 8)
-        myCat = Cat("Birman Cat", "Felix", 3)
+    def __init__(self, givenbread, givenname, givenage):
+        self.bread = givenbread
+        number_of_legs = 4  # all cats have 4 legs, so set as a constant
+        super().__init__(givenname, givenage)
 
-        #print("the dog: " +myDog.species + " ... \n" + myDog.describe(),
-        #"\nthe cat: " +myCat.bread + " says ...\n  " + myCat.describe())
-        #myDog.speak("hello everyone I can Bark .. ")
-        #myDog.bark()
-        #myDog.sleep()
-        #myCat.meow()
-        #myCat.sleep()
-        #myDog.ask(myCat.meow())    # interaction
-        #myCat.ask(myDog.bark())    # interaction
+    def meow(self):
+        print(" Meow .... meow..... Meow")
 
-        ############################################
-        # trying methods which are not exist
-        #print(myDog.colour)
-        #myDog.breathe()
-        ############################################
+    def sleep(self):  # This will override the sleep method from the super class for ONLY the CAT
+        print("my name is  " + self.name +
+              " I am closing my eyes slowly and ... Snore....Snore....Snore")  # myCat.sleep() != myDog.sleep()
 
-        ############################################
-       # using setter and getter
-        #print("my age before is " + str(myDog.get_age()))
-        #myDog.set_age(6)
-        #print("now my age is " +str(myDog.get_age()))
 
-        #############################################
+if __name__ == '__main__':
+    myDog = Dog("Bull dog", "Max", 8)
+    myCat = Cat("Birman Cat", "Felix", 3)
+
+    print("the dog: " + myDog.species + " ... \n" + myDog.describe(),
+          "\nthe cat: " + myCat.bread + " says ...\n  " + myCat.describe())
+    myDog.speak("hello everyone I can Bark .. ")
+    myDog.bark()
+    myDog.sleep()
+    myCat.meow()
+    myCat.sleep()
+    myDog.ask(myCat.meow())  # interaction - Dog asks cat to meow
+    myCat.ask(myDog.bark())  # interaction - Cat asks dog to bark         #
+    # Interactions must be done in terms of the methods known to the objects
+
+    ############################################
+    # trying methods which are not exist
+    # print(myDog.colour)
+    # myDog.breathe()
+    ############################################
+
+    ############################################
+    # using setter and getter
+    print("my age before is " + str(myDog.get_age()))
+    myDog.set_age(6)
+    print("now my age is " + str(myDog.get_age()))
+    #############################################
--- Others/Object Oriented Programming/Worksheet 2/Shapes.py
+++ Others/Object Oriented Programming/Worksheet 2/Shapes.py
@@ -8,13 +8,18 @@ class shapes():
     # Methods
 
     def rotate(self, angle):
-        print("Rotated by "+angle)
+        print("Rotated by " + str(angle))
 
     def copy(self):
         print("Copied")
 
     def enlarge(self, percentage):
-        print("Enlarged by "+ percentage + "%")
+        print("Enlarged by "+ str(percentage) + "%")
+
+    def outputComFeatures(self):
+        print(self.fillColour)
+        print(self.outlineColour)
+        print(self.outlineThickness)
 
 
 class circle(shapes):
@@ -27,6 +32,11 @@ class circle(shapes):
     def changeOutlineColour(self, new):
         self.outlineColour = new
 
+    def outputCirFeatures(self):
+        self.outputComFeatures()
+        print(self.radius)
+        print(self.centre)
+
 
 class rectangle(shapes):
     def __init__(self, FillC, OutC, OutT, Len, wid):
@@ -37,8 +47,28 @@ class rectangle(shapes):
     def addText(self, text):
         print(text)
 
+    def outputRecFeatures(self):
+        self.outputComFeatures()
+        print(self.length)
+        print(self.width)
+
 
 circle_1 = circle("green", "red", 10, 3, (0,0))
 circle_2 = circle("blue", "pink", 4, 5, (0,5))
 rectangle_1 = rectangle("green", "yellow", 6, 5, 3)
 rectangle_2 = rectangle("brown", "blue", 3, 10, 5)
+circle_1.enlarge(50)
+rectangle_1.rotate(30)
+rectangle_1.addText("I am using OOP")
+circle_2.changeOutlineColour("Purple")
+print("Circle1 features:")
+circle_1.outputCirFeatures()
+print()
+print("Circle2 features:")
+circle_2.outputCirFeatures()
+print()
+print("Rectangle1 Features:")
+rectangle_1.outputRecFeatures()
+print()
+print("Rectangle1 Features:")
+rectangle_1.outputRecFeatures()
